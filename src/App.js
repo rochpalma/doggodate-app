8dba18a5151926a8ffa35514665c2be28837ac5b
Rochelle Palma <palmarochelleg@gmail.com>
2020-07-19 23:16:13 -0700

    signup working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import Signin from './components/Signin/Signin';
 import Signup from './components/Signup/Signup';
@@ -8,23 +8,31 @@ import ProfilePage from './components/ProfilePage/ProfilePage';
 import Feed from './components/Feed/Feed';
 import SelectedProfile from './components/SelectedProfile/SelectedProfile';
 import Meetup from './components/Meetup/Meetup';
+import Context from "./Context";
 import  './App.css';
 
 
-function App() {
-  return (
-    <div className='App'>
-      <Route exact path = '/' component={LandingPage}/>
-      <Route exact path = '/signin' component={Signin}/>
-      <Route exact path = '/signup' component={Signup}/>
-      <Route exact path = '/setup' component={ProfileSetup}/>
-      <Route exact path = '/myprofile' component={ProfilePage}/>
-      <Route exact path = '/feed' component={Feed}/>
-      <Route exact path = '/logout' component={LandingPage}/>
-      <Route exact path = '/dog-1' component={SelectedProfile}/>
-      <Route exact path = '/meetup' component={Meetup}/>
-    </div>
-  );
-}
+export default class App extends Component {
+  static contextType = Context;
 
-export default App;
+  state = {
+    user: {},
+    dogs: []
+  }
+
+  render(){
+    return (
+      <div className='App'>
+        <Route exact path = '/' component={LandingPage}/>
+        <Route exact path = '/signin' component={Signin}/>
+        <Route exact path = '/signup' component={Signup}/>
+        <Route exact path = '/setup' component={ProfileSetup}/>
+        <Route exact path = '/myprofile' component={ProfilePage}/>
+        <Route exact path = '/feed' component={Feed}/>
+        <Route exact path = '/logout' component={LandingPage}/>
+        <Route exact path = '/dog-1' component={SelectedProfile}/>
+        <Route exact path = '/meetup' component={Meetup}/>
+      </div>
+    );
+  }
+}
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,29 +1,69 @@
 import React, { Component } from 'react';
+import Context from "../../Context";
 import HomeNav from '../HomeNav/HomeNav';
 import Footer from '../Footer/Footer';
 import { Link } from 'react-router-dom';
 import './Signup.css';
+import AuthApiService from '../../services/auth-api-service';
 
 class Signup extends Component {
-    render() { 
+    static contextType = Context;
+
+    state = {
+        error: null
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const { first_name, last_name, email, password } = event.target;
+
+        this.setState({ error: null });
+        AuthApiService.postUser({
+            full_name: first_name.value +' '+ last_name.value,
+            email: email.value,
+            password: password.value,
+        })
+        .then(user => {
+            first_name.value = ''
+            last_name.value = ''
+            email.value = ''
+            password.value = ''
+            this.props.history.push('/signin')
+        })
+        .catch(err => {
+            this.setState({ error: err.error.message })
+        })
+    }
+
+    renderInvalidMessage = () => {
+        return <p>{this.state.error}</p>
+    }
+
+    render() {
+        const { error } = this.state; 
         return (  
             <div className='main-display signup-bg'>
                 <HomeNav />
                 <main>
                     <div className='userform-container'>
-                        <form className='form-border'>                 
+                        <form 
+                            className='form-border'
+                            onSubmit={this.handleSubmit}
+                        > 
+                        {(error) ? this.renderInvalidMessage() : null}                
                             <div className='form-fields'>
-                                <label for='first-name'>First Name</label>
-                                <input type='text' name='first-name' id='first-name' required/>
-                                <label for='last-name'>Last Name</label>
-                                <input type='text' name='last-name' id='last-name' required/>
-                                <label for='email-add'>Email Address</label>
-                                <input type='email' name='email-add' id='email-add' required/>
+                                <label for='first_name'>First Name</label>
+                                <input type='text' name='first_name' id='first_name' required/>
+                                <label for='last_name'>Last Name</label>
+                                <input type='text' name='last_name' id='last_name' required/>
+                                <label for='email'>Email Address</label>
+                                <input type='email' name='email' id='email' required/>
                                 <label for='password'>Password</label>
                                 <input type='password' name='password' id='password' required/>
                                 <label for='password-2'>Confirm Password</label>
                                 <input type='password' name='password-2' id='password-2' required className='red'/>
-                                <Link to='/setup' className='btn'>Sign up</Link>
+                                {/* <Link to='/setup' className='btn'>Sign up</Link> */}
+                                <button type='submit' className='btn'>Sign up</button>
                             </div>
                         </form>
                     </div>
