81dec8114716a8c10cbb4b384aef1fc93357cb69
Rochelle Palma <palmarochelleg@gmail.com>
2020-07-21 23:30:42 -0700

    call function to save the user id

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -5,6 +5,7 @@ import Footer from '../Footer/Footer';
 import { Link } from 'react-router-dom';
 import './Signup.css';
 import AuthApiService from '../../services/auth-api-service';
+import TokenService from '../../services/token-service';
 
 class Signup extends Component {
     static contextType = Context;
@@ -23,11 +24,12 @@ class Signup extends Component {
             email: email.value,
             password: password.value,
         })
-        .then(user => {
+        .then(res => {
             first_name.value = ''
             last_name.value = ''
             email.value = ''
             password.value = ''
+            TokenService.saveUserId(res.id)
             this.props.history.push('/setup')
         })
         .catch(err => {
@@ -52,15 +54,15 @@ class Signup extends Component {
                         > 
                         {(error) ? this.renderInvalidMessage() : null}                
                             <div className='form-fields'>
-                                <label for='first_name'>First Name</label>
+                                <label htmlFor='first_name'>First Name</label>
                                 <input type='text' name='first_name' id='first_name' required/>
-                                <label for='last_name'>Last Name</label>
+                                <label htmlFor='last_name'>Last Name</label>
                                 <input type='text' name='last_name' id='last_name' required/>
-                                <label for='email'>Email Address</label>
+                                <label htmlFor='email'>Email Address</label>
                                 <input type='email' name='email' id='email' required/>
-                                <label for='password'>Password</label>
+                                <label htmlFor='password'>Password</label>
                                 <input type='password' name='password' id='password' required/>
-                                <label for='password-2'>Confirm Password</label>
+                                <label htmlFor='password-2'>Confirm Password</label>
                                 <input type='password' name='password-2' id='password-2' required className='red'/>
                                 {/* <Link to='/setup' className='btn'>Sign up</Link> */}
                                 <button type='submit' className='btn'>Sign up</button>
