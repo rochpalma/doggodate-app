fb85f632a5f7dff916ac30ddb9422532f9500274
Rochelle Palma <palmarochelleg@gmail.com>
2020-07-29 22:26:01 -0700

    temporarily commented out some lines

--- src/App.js
+++ src/App.js
@@ -12,19 +12,16 @@ import Context from "./Context";
 import  './App.css';
 import TokenService from './services/token-service';
 import DoggodateApiService from './services/doggodate-api-service';
+import { DogsProvider } from './Context';
 
 export default class App extends Component {
-  static contextType = Context;
+  // static contextType = Context;
 
-  state = {
-    user: {},
-    dogs: []
-  }
+  // state = {
+  //   user: {},
+  //   dogs: []
+  // }
 
-  //to be completed
-  getUserData = (user) => {
-  
-  };
 
   //try
   // componentDidMount() {
@@ -41,17 +38,19 @@ export default class App extends Component {
 
   render(){
     return (
-      <div className='App'>
-        <Route exact path = '/' component={LandingPage}/>
-        <Route exact path = '/signin' component={Signin}/>
-        <Route exact path = '/signup' component={Signup}/>
-        <Route exact path = '/setup' component={ProfileSetup}/>
-        <Route exact path = '/myprofile' component={ProfilePage}/>
-        <Route exact path = '/feed' component={Feed}/>
-        <Route exact path = '/logout' component={LandingPage}/>
-        <Route exact path = '/dog-1' component={SelectedProfile}/>
-        <Route exact path = '/meetup' component={Meetup}/>
-      </div>
+      <DogsProvider>
+        <div className='App'>
+          <Route exact path = '/' component={LandingPage}/>
+          <Route exact path = '/signin' component={Signin}/>
+          <Route exact path = '/signup' component={Signup}/>
+          <Route exact path = '/setup' component={ProfileSetup}/>
+          <Route exact path = '/myprofile' component={ProfilePage}/>
+          <Route exact path = '/feed' component={Feed}/>
+          <Route exact path = '/logout' component={LandingPage}/>
+          <Route exact path = '/dogs/:id' component={SelectedProfile}/>
+          <Route exact path = '/meetup' component={Meetup}/>
+        </div>
+      </DogsProvider>
     );
   }
 }
