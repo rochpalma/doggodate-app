f65f872b4e8b9ca22eea4c0056f3a58b254cedf2
Rochelle Palma <palmarochelleg@gmail.com>
2020-08-10 02:08:05 -0700

    make comment works

--- src/components/CommentSection/CommentSection.js
+++ src/components/CommentSection/CommentSection.js
@@ -1,32 +1,9 @@
 import React, { Component } from 'react';
-import Context from '../../Context';
 import Comments from './Comments';
 import PostComment from './PostComment';
-import DoggodateApiService from '../../services/doggodate-api-service';
-import TokenService from '../../services/token-service';
 import './Comments.css'
 
 class CommentSection extends Component {
-    // static contextType = Context;
-
-    // state = {
-    //     comments: this.context.comments,
-    // }
-
-    // componentDidMount() {
-    //     //const profile_id = TokenService.getDogId();
-    //     DoggodateApiService.getComments(this.props.profile_id)
-    //         .then(res => {
-
-    //             //this.context.setComments(res);
-    //             console.log("context" +JSON.stringify(res))
-                
-                
-    //         })
-    //         // .then(this.context.setComments)
-    //         .catch(this.context.setError);
-    
-
     render() {
         const comments = this.props.comments.map((comment, index) => {
             return(
@@ -35,6 +12,7 @@ class CommentSection extends Component {
                     id={comment.id}
                     user_id={comment.user_id}
                     content={comment.content}
+                    full_name={comment.full_name}
                 />
             )
         }) 
